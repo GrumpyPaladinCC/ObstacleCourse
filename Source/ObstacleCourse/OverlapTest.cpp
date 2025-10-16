3ef6afebde936c217b7484c4c98bb2778c044baa
GrumpyPaladin <georgieiordanou@gmail.com>
2025-10-16 12:36:51 +0300

    code
    code

--- Source/ObstacleCourse/OverlapTest.cpp
+++ Source/ObstacleCourse/OverlapTest.cpp
@@ -2,6 +2,7 @@
 
 
 #include "OverlapTest.h"
+#include "Components/BoxComponent.h"
 
 // Sets default values
 AOverlapTest::AOverlapTest()
@@ -9,13 +10,28 @@ AOverlapTest::AOverlapTest()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger"));
+
+	RootComponent = trigger;
+
+	trigger->SetGenerateOverlapEvents(true);
 }
 
 // Called when the game starts or when spawned
 void AOverlapTest::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("Test"));
+
+
+
+		trigger->OnComponentBeginOverlap.AddDynamic(this, &AOverlapTest::OnOverlapStart);
+		UE_LOG(LogTemp, Warning, TEXT("Trigger successfully added"));
+}
+
+void AOverlapTest::OnOverlapStart(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	PrintDebug(FString::Printf(TEXT("Overlap with %s"), *OtherActor->GetName()), FColor::Yellow, 5.f);
+	Destroy();
 }
 
 // Called every frame
@@ -23,5 +39,13 @@ void AOverlapTest::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AOverlapTest::PrintDebug(const FString& Message, FColor Color, float Time)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, Time, Color, Message);
+	}
 }
 
--- Source/ObstacleCourse/OverlapTest.h
+++ Source/ObstacleCourse/OverlapTest.h
@@ -10,8 +10,8 @@ UCLASS()
 class OBSTACLECOURSE_API AOverlapTest : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AOverlapTest();
 
@@ -19,8 +19,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	void OnOverlapStart(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+		);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* trigger;
+
+	void PrintDebug(const FString& Message, FColor Color = FColor::White, float Time = 2.f);
 };
