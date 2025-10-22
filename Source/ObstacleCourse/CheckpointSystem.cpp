88d670e4339c5d40de8f60684d2d9611a739553e
GrumpyPaladin <georgieiordanou@gmail.com>
2025-10-22 12:36:53 +0300

    Respawn System

--- Source/ObstacleCourse/CheckpointSystem.cpp
+++ Source/ObstacleCourse/CheckpointSystem.cpp
@@ -2,6 +2,8 @@
 
 
 #include "CheckpointSystem.h"
+#include "Components/BoxComponent.h"
+#include "ObstacleCourseCharacter.h"
 
 // Sets default values
 ACheckpointSystem::ACheckpointSystem()
@@ -9,13 +11,29 @@ ACheckpointSystem::ACheckpointSystem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger"));
+	RootComponent = trigger;
+
+	RespawnPoint = GetActorLocation();
 }
 
 // Called when the game starts or when spawned
 void ACheckpointSystem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	trigger->OnComponentBeginOverlap.AddDynamic(this, &ACheckpointSystem::overlapStart);
+	RespawnPoint = GetActorLocation();
+	UE_LOG(LogTemp, Warning, TEXT("2ND Trigger successfully added"));
+}
+
+void ACheckpointSystem::overlapStart(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AObstacleCourseCharacter* Player = Cast<AObstacleCourseCharacter>(OtherActor);
+	if (Player)
+	{
+		Player->RespawnLocation = RespawnPoint;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Checkpoint reached, new respawn point set"));
 }
 
 // Called every frame
